b56779389bcbdb0539c607ee1b2a46e1d053eaf5
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-04-06 09:44:31 -0700

    Add logic to return a single selected topping.

--- js/scripts.js
+++ js/scripts.js
@@ -10,9 +10,11 @@ $(document).ready(function() {
     event.preventDefault();
 
     var inputSize = $("#size").val();
+    var inputToppings = $("input:checkbox[name=toppings]:checked").val();
 
     var newPizza = new Pizza(inputSize);
 
     $(".receipt .size").text(newPizza.size);
+    $(".receipt ul").append("<li>" + inputToppings + "</li>");
   });
 });
